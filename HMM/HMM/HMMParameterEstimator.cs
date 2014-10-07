b23c8147bff3307b6b52e4a9e8c70159ddd01704
Richard Klafter <rpklafter@yahoo.com>
2014-10-07 15:30:27 +0700

    added UpdateHMM function

--- HMM/HMM/HMMParameterEstimator.cs
+++ HMM/HMM/HMMParameterEstimator.cs
@@ -22,37 +22,81 @@ namespace HMM
             BackwardFunc = Parent.BackwardFunc(OutputSequence);
             ProbabilityOfOutput = Util.Range(Parent.States.Count)
                 .Select(state => ForwardFunc(OutputSequence.Length, state))
-                    .LogSum();
+                .LogSum();
         }
         //public double ProbabilityOfOutput(int time) { return Parent.States.Select((trash, state) => ForwardFunc(time, state) * BackwardFunc(time, state)).LogSum(); }
 
+        public void UpdateHMM()
+        {
+            //Calculate new model variables
+            double[] newIntialStateProbabilities = Parent.IntialStateProbabilities.Select(
+                (trash, state) => ExpectedNumberOfTransitions(0, state).Exp()
+            ).ToArray();
+            double[][] newTransitionProbabilities = Parent.StateTransitionProbabilities
+                .EnumerateRows()
+                .Select(
+                    (row, fromState) =>
+                    {
+                        return row.Select(
+                            (oldProb, toState) => (TotalExpectedNumberOfTransitions(fromState, toState) - TotalExpectedNumberOfTransitions(fromState)).Exp()
+                        ).ToArray();
+                    }
+                ).ToArray();
+            double[][][] newSymbolEmissionProbabilities = Parent.SymbolEmissionProbabilities
+                .Select((matrix, fromState) =>
+                    matrix.EnumerateRows().Select(
+                        (row, toState) =>
+                        {
+                            return row.Select(
+                                (oldProb, symbol) => (TotalExpectedNumberOfTransitions(fromState, toState, symbol) - TotalExpectedNumberOfTransitions(fromState, toState)).Exp()
+                            ).ToArray();
+                        }
+                   ).ToArray()
+                ).ToArray();
 
+            //Update the model!
+            Parent.IntialStateProbabilities.SetValues(newIntialStateProbabilities.ToArray());
+            newTransitionProbabilities.ForEach(
+                (row, fromState) => Parent.StateTransitionProbabilities.SetRow(fromState, row)
+            );
+            newSymbolEmissionProbabilities.ForEach(
+                (matrix, fromState) => matrix.ForEach(
+                    (row, toState) => Parent.SymbolEmissionProbabilities[fromState].SetRow(toState, row)
+                )
+           );     
+        }
         public double ExpectedNumberOfTransitions(int time, int fromState, int toState)
         {
             return ForwardFunc(time, fromState) 
                 + Parent.StateTransitionProbabilities[fromState, toState].Log()
-                    + Parent.SymbolEmissionProbabilities[fromState][toState, OutputSequence[time]].Log()
-                    + BackwardFunc(time, toState)
-                    - ProbabilityOfOutput;
+                + Parent.SymbolEmissionProbabilities[fromState][toState, OutputSequence[time]].Log()
+                + BackwardFunc(time, toState)
+                - ProbabilityOfOutput;
+        }
+        public double TotalExpectedNumberOfTransitions(int fromState, int toState, int outputedSymbol)
+        {
+            return Util.Range(OutputSequence.Length)
+                .Where(time => OutputSequence[time] == outputedSymbol)
+                .Select((time) => ExpectedNumberOfTransitions(time, fromState, toState))
+                .LogSum();
         }
         public double TotalExpectedNumberOfTransitions(int fromState, int toState)
         {
-            return OutputSequence
-                .Select((trash, time) => ExpectedNumberOfTransitions(time, fromState, toState))
-                    .LogSum();
+            return Util.Range(OutputSequence.Length)
+                .Select((time) => ExpectedNumberOfTransitions(time, fromState, toState))
+                .LogSum();
         }
-
-        public double ExpectedNumberOfTransitions(int time, int toState)
+        public double ExpectedNumberOfTransitions(int time, int fromState)
         {
-            return Parent.States
-                .Select((trash, state) => ExpectedNumberOfTransitions(time, state, toState))
-                    .LogSum();
+            return Parent.States.Values
+                .Select(state => ExpectedNumberOfTransitions(time, fromState, state))
+                .LogSum();
         }
-        public double TotalExpectedNumberOfTransitions(int toState)
+        public double TotalExpectedNumberOfTransitions(int fromState)
         {
             return OutputSequence
-                .Select((trash, time) => ExpectedNumberOfTransitions(time, toState))
-                    .LogSum();
+                .Select((trash, time) => ExpectedNumberOfTransitions(time, fromState))
+                .LogSum();
         }            
     }
 }
